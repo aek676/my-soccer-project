fd7b414cbafb44e10963ea96dc270238edbc6b37
aek676 <aek676@inlumine.ual.es>
2026-05-04 01:42:26 +0200

    feat: add players-service microservice

---
+++ players-service/src/main/java/es/ual/players_service/PlayersServiceApplication.java
@@ -0,0 +1,13 @@
+package es.ual.players_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlayersServiceApplication.class, args);
+  }
+
+}
