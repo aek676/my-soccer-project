501375d8a6fb7bb5b3ab7ebf111e906e1b214919
aek676 <aek676@inlumine.ual.es>
2026-05-05 15:40:38 +0200

    feat(terraform): add Azure resources and azapi provider
    Add Azure resource group, log analytics workspace, container app
    environment, and Eureka server using azapi provider. Update providers,
    variables, and outputs to support Azure deployment alongside GCP.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,16 @@ output "project_number" {
   description = "GCP Project Number"
   value       = module.gcp.project_number
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "container_app_environment_id" {
+  value = azurerm_container_app_environment.env.id
+}
+
+output "eureka_server_id" {
+  value = azapi_resource.eureka_server.id
+}
+
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.70.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.9.0"
+    }
     google = {
       source  = "hashicorp/google"
       version = "7.30.0"
@@ -15,6 +19,8 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+provider "azapi" {}
 
 provider "google" {
 }
