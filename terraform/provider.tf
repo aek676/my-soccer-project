e734cebf1cfd208f811d8c2551f1872e48eb0db3
aek676 <aek676@inlumine.ual.es>
2026-05-05 19:46:21 +0200

    feat(terraform): add azapi provider support
    Added Azure azapi provider version 2.9.0 to Terraform configuration and
    lock file. Updated provider.tf to include azapi provider block for
    extended Azure resource management capabilities.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,17 +4,22 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.70.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.9.0"
+    }
     google = {
       source  = "hashicorp/google"
       version = "7.30.0"
     }
   }
-
 }
 
 provider "azurerm" {
   features {}
 }
+
+provider "azapi" {}
 
 provider "google" {
 }
