f5aed433b64a823194b21d8eabc8fa25d26be470
aek676 <aek676@inlumine.ual.es>
2026-05-07 01:33:52 +0200

    feat(terraform): add config server and modules
    - Added azapi_resource for Spring Cloud Config Server.
    - Introduced modules for microservices and gateway deployment.
    - Updated variables for config server URI, app name, image, and
    services.
    - Changed required Terraform version constraint for flexibility.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,32 @@
 variable "location" {
+  type        = string
   default     = "norwayeast"
   description = "The Azure region to deploy resources in"
 }
 
 variable "prefix" {
+  type        = string
   default     = "my-soccer-project"
   description = "A prefix for naming resources"
 }
 
+variable "config_server_uri" {
+  type        = string
+  description = "URI for the Spring Cloud Config Server Git repository"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Name of the container app"
+}
+
+variable "image" {
+  type        = string
+  description = "Docker image for the container app"
+}
+
+variable "services" {
+  type        = list(string)
+  default     = ["comments-service", "ideal-team-service", "players-service"]
+  description = "List of microservice names to deploy"
+}
