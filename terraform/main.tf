0b61aac67e5dc1745892b93447c6ac4356d6f631
aek676 <aek676@inlumine.ual.es>
2026-05-13 18:01:07 +0200

    feat(terraform): add news_service module deployment
    Add a new "news_service" module to main.tf for deploying the
    news-service microservice. Configures resource dependencies and GCP
    artifact registry credentials.

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,6 +86,19 @@ module "bun_backend" {
   mongo_atlas_uri       = var.mongo_atlas_uri
 }
 
+module "news_service" {
+  source                = "./modules/microservice"
+  app_name              = "news-service"
+  image                 = var.image
+  eureka_server_id      = azapi_resource.eureka_server.id
+  config_server_id      = azapi_resource.config_server.id
+  resource_group_id     = azurerm_resource_group.rg.id
+  environment_id        = azurerm_container_app_environment.env.id
+  location              = azurerm_resource_group.rg.location
+  gcp_registry_username = "_json_key_base64"
+  gcp_registry_password = module.gcp.artifact_registry_json_key
+}
+
 module "gateway" {
   source                = "./modules/microservice"
   app_name              = "api-gateway"
@@ -99,4 +112,3 @@ module "gateway" {
   gcp_registry_username = "_json_key_base64"
   gcp_registry_password = module.gcp.artifact_registry_json_key
 }
-
