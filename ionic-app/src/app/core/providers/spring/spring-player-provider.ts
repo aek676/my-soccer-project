c4cecbc06749680cf7f741236ca708a585e30790
aek676 <aek676@inlumine.ual.es>
2026-05-26 19:48:17 +0200

    feat(infra): add init SQL seed, docker-compose volume, and update frontend endpoint

--- ionic-app/src/app/core/providers/spring/spring-player-provider.ts
+++ ionic-app/src/app/core/providers/spring/spring-player-provider.ts
@@ -3,18 +3,19 @@ import { PlayerModel } from '@core/models/player.model';
 import { BaseProvider } from '../base-provider';
 import { PlayerProviderInterface } from '../player-provider.interface';
 
-// TODO: modify the endpoint to match the actual backend API for players
 export class SpringPlayerProvider
   extends BaseProvider
   implements PlayerProviderInterface
 {
   getPlayerById(playerId: string): Observable<PlayerModel> {
     return this.http.get<PlayerModel>(
-      `${this.gatewayUrl}/players-spring/${playerId}`,
+      `${this.gatewayUrl}/players-service/players/${playerId}`,
     );
   }
 
   getPlayers(): Observable<PlayerModel[]> {
-    return this.http.get<PlayerModel[]>(`${this.gatewayUrl}/players-spring`);
+    return this.http.get<PlayerModel[]>(
+      `${this.gatewayUrl}/players-service/players`,
+    );
   }
 }
