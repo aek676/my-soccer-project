523a40847a868ae3894a3a21563acbf8fe36171e
aek676 <aek676@inlumine.ual.es>
2026-05-26 16:42:08 +0200

    feat: add IdealTeam model

---
+++ bun-backend/src/models/ideal-team.ts
@@ -0,0 +1,18 @@
+import { Schema, model, InferSchemaType } from "mongoose";
+
+const IdealTeamSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    created: { type: Date, default: Date.now },
+    idUser: { type: String, required: true, index: true },
+    players: [{ type: Schema.Types.ObjectId, ref: "Player" }],
+  },
+  {
+    timestamps: false,
+    versionKey: false,
+    collection: "idealTeams",
+  },
+);
+
+export type IdealTeam = InferSchemaType<typeof IdealTeamSchema>;
+export const IdealTeam = model("IdealTeam", IdealTeamSchema);
