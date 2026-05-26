b0187784545f8173e9bdbb6c804b7f3d622d36e8
aek676 <aek676@inlumine.ual.es>
2026-05-26 16:42:03 +0200

    feat: add Player model

---
+++ bun-backend/src/models/player.ts
@@ -0,0 +1,39 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const PlayerSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    age: { type: Number },
+    birthdate: { type: Date },
+    nationality: { type: String },
+    height: { type: String },
+    weight: { type: String },
+    number: { type: Number },
+    team: { type: String },
+    league: { type: String },
+    position: { type: String },
+    photo: { type: String },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    created: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: false,
+    versionKey: false,
+    collection: "players",
+  },
+);
+
+export type Player = InferSchemaType<typeof PlayerSchema>;
+export const Player = model("Player", PlayerSchema);
