f3a073298f85a41b6183fd46fb901a7d91638e03
Anass El Jabiry Kaddir <aek676@inlumine.ual.es>
2026-05-26 18:10:53 +0200

    Update bun-backend/src/modules/player/service.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- bun-backend/src/modules/player/service.ts
+++ bun-backend/src/modules/player/service.ts
@@ -2,15 +2,15 @@ import { Player } from "../../models/player";
 import type { PlayerResponseType } from "./model";
 
 export abstract class PlayerService {
-  static async getAllPlayers(): Promise<PlayerResponseType[] | Error> {
-    try {
-      const players = await Player.find({}).lean();
-      return players.map((p) => ({
-        ...p,
-        _id: p._id.toString(),
-      })) as PlayerResponseType[];
-    } catch (_error) {
-      return new Error("Failed to fetch players");
-    }
-  }
+	static async getAllPlayers(): Promise<PlayerResponseType[] | Error> {
+		try {
+			const players = await Player.find({}).lean();
+			return players.map((p) => ({
+				...p,
+				_id: p._id.toString(),
+			})) as PlayerResponseType[];
+		} catch (_error) {
+			return new Error("Failed to fetch players");
+		}
+	}
 }
