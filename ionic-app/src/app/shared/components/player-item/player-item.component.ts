75d819cea4d95be3cf449ebe75c5898e53d92624
aek676 <aek676@inlumine.ual.es>
2026-05-28 00:04:11 +0200

    feat(ionic): add selectable mode to player-item and back button to shared-header

--- ionic-app/src/app/shared/components/player-item/player-item.component.ts
+++ ionic-app/src/app/shared/components/player-item/player-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {
   IonItem,
   IonAvatar,
   IonLabel,
   IonIcon,
+  IonCheckbox,
 } from '@ionic/angular/standalone';
 import { person, chevronForward } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
@@ -13,12 +14,19 @@ import { PlayerModel } from '@core/models/player.model';
   selector: 'app-player-item',
   templateUrl: './player-item.component.html',
   styleUrls: ['./player-item.component.scss'],
-  imports: [IonItem, IonAvatar, IonLabel, IonIcon],
+  imports: [IonItem, IonAvatar, IonLabel, IonIcon, IonCheckbox],
 })
 export class PlayerItemComponent {
   @Input() player!: PlayerModel;
+  @Input() selectable = false;
+  @Input() selected = false;
+  @Output() selectionChange = new EventEmitter<boolean>();
 
   constructor() {
     addIcons({ person, chevronForward });
+  }
+
+  onCheckboxChange(event: any) {
+    this.selectionChange.emit(event.detail.checked);
   }
 }
