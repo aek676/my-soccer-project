05e9091e88786acc44cb417b84b963b293efb6a1
aek676 <aek676@inlumine.ual.es>
2026-05-28 00:04:11 +0200

    feat(ionic): add selectable mode to player-item and back button to shared-header

--- ionic-app/src/app/shared/components/shared-header/shared-header.component.ts
+++ ionic-app/src/app/shared/components/shared-header/shared-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import {
   IonToolbar,
   IonTitle,
@@ -8,7 +8,7 @@ import {
 import { Router } from '@angular/router';
 import { AuthStateService } from '@core/services/auth-state.service';
 import { AsyncPipe } from '@angular/common';
-import { logIn, football } from 'ionicons/icons';
+import { logIn, football, arrowBack } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 
 @Component({
@@ -22,10 +22,17 @@ export class SharedHeaderComponent {
   private authState = inject(AuthStateService);
   private router = inject(Router);
 
+  @Input() showBackButton = false;
+  @Output() back = new EventEmitter<void>();
+
   isGuest$ = this.authState.isGuest$;
 
   constructor() {
-    addIcons({ logIn, football });
+    addIcons({ logIn, football, arrowBack });
+  }
+
+  goBack() {
+    this.back.emit();
   }
 
   goToLogin() {
