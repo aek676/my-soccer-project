5669dabdbe82f6ce03db9b79893bf38a4a05b454
aek676 <aek676@inlumine.ual.es>
2026-05-31 01:30:13 +0200

    test: enable Testcontainers for PostgreSQL in tests
    Add Testcontainers dependencies and configure
    CommentsServiceApplicationTests
    to use a PostgreSQL container for integration testing. Disable config
    server
    and Eureka for test context.

--- comments-service/src/test/java/es/ual/comments_service/CommentsServiceApplicationTests.java
+++ comments-service/src/test/java/es/ual/comments_service/CommentsServiceApplicationTests.java
@@ -2,12 +2,24 @@ package es.ual.comments_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.cloud.config.enabled=false",
+    "eureka.client.enabled=false"
+}, useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@Testcontainers
 class CommentsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Container
+  @ServiceConnection
+  static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18.3-alpine3.23");
+
+  @Test
+  void contextLoads() {
+  }
 
 }
