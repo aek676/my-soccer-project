31954596cbbf7c799db708057981e88867ee67b5
aek676 <aek676@inlumine.ual.es>
2026-06-01 04:41:48 +0200

    test(ionic-app): add spec files and expand tests to increase branch coverage above 80%

--- ionic-app/src/app/core/interceptors/auth-interceptor.spec.ts
+++ ionic-app/src/app/core/interceptors/auth-interceptor.spec.ts
@@ -1,17 +1,52 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { Auth } from '@angular/fire/auth';
 import { authInterceptor } from './auth-interceptor';
 
 describe('authInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+  let httpMock: HttpTestingController;
+  let mockGetIdToken: jasmine.Spy;
+  const mockAuth = { currentUser: null } as any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockGetIdToken = jasmine.createSpy('getIdToken').and.resolveTo('test-token');
+    mockAuth.currentUser = { getIdToken: mockGetIdToken };
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([authInterceptor])),
+        provideHttpClientTesting(),
+        { provide: Auth, useValue: mockAuth },
+      ],
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   });
+
+  it('should add Authorization header when user is authenticated', fakeAsync(() => {
+    TestBed.inject(HttpClient).get('/test').subscribe();
+    tick();
+
+    const req = httpMock.expectOne('/test');
+    expect(req.request.headers.get('Authorization')).toBe('Bearer test-token');
+    expect(mockGetIdToken).toHaveBeenCalledWith(true);
+    req.flush({});
+  }));
+
+  it('should pass request through when no user is authenticated', fakeAsync(() => {
+    mockAuth.currentUser = null;
+
+    TestBed.inject(HttpClient).get('/test').subscribe();
+    tick();
+
+    const req = httpMock.expectOne('/test');
+    expect(req.request.headers.has('Authorization')).toBeFalse();
+    req.flush({});
+  }));
 });
